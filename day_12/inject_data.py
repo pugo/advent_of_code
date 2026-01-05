27d9453a2f92a3ff7eac75046ea8cc6ca24ff528
Anders Piniesjö <anders.piniesjo@ctek.com>
2026-01-05 01:12:35 +0100

    Fixed day 12 data injector. Updated comments.

--- day_12/inject_data.py
+++ day_12/inject_data.py
@@ -7,20 +7,70 @@ import data_injector
 
 class Injector(data_injector.DataInjector):
     def parse_data(self) -> None:
-        lines = [l for l in self._data.split('\n') if l]
-        self._add_dataset('numbers', lines)
+        shapes = []
+        regions = []
 
-    def generate_data(self) -> str:
+        read_shapes = True
+        for line in self._data.split('\n'):
+            if 'x' in line:
+                read_shapes = False
+
+            if read_shapes:
+                shapes.append(line)
+            else:
+                regions.append(line)
+
+        self._add_dataset('shapes', shapes)
+        self._add_dataset('regions', regions)
+
+    def generate_shapes_data(self) -> str:
         lines = []
 
-        for line in self._datasets['numbers']:
+        shape_index = None
+        shape_lines = []
+        for line in self._datasets['shapes']:
+            if ':' in line:
+                lines.append(f'{line[:-1]} [')
+                continue
+
+            if not line:
+                lines.append(']')
+                lines.append('')
+                continue
+
             lines.append(f'({line})')
 
         return '\n'.join(lines)
 
+    # Generate regions as: [[4 4] [0 0 0 0 2 0]]
+    def generate_regions_data(self) -> str:
+        items_per_line = 4
+        lines = []
+        line = []        
+
+        for l in self._datasets['regions']:
+            if not l:
+                continue
+
+            p = l.split(':')
+            w, l = p[0].split('x')
+            print(p, w, l)
+
+            line.append(f'[[{w} {l}] [{p[1].strip()}]]')
+            if len(line) == items_per_line:
+                lines.append(' '.join(line))
+                line = []
+
+        if line:
+            lines.append(' '.join(line))
+
+        return '\n'.join(lines)
+
     TAG_INJECTORS = {
-        '<<INJECT DATA HERE>>': generate_data,
+        '<<INJECT SHAPES DATA HERE>>': generate_shapes_data,
+        '<<INJECT REGIONS DATA HERE>>': generate_regions_data,
     }
+
 
 
 if __name__ == '__main__':
